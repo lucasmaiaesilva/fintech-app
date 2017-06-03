5b56d17dfd6c99c0d9fea2b64dc0c7b8567ed8b4
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2017-06-03 16:20:53 -0300

    rewriting the footer test to go deep

--- tests/Footer.test.js
+++ tests/Footer.test.js
@@ -1,14 +1,26 @@
-// import React from 'react'
-// import {shallow} from 'enzyme'
-// import {expect} from 'chai'
-// import Footer from '../src/components/Footer'
-//
-// describe('<Footer />', () => {
-//   it('rendered the author props', () => {
-//     const wrapper = shallow(<Footer author='unique' />)
-//     expect(wrapper.text()).to.contain('unique')
-//   })
-//   it('Is propTypes name exist', () => {
-//     expect(Footer.propTypes).to.have.property('author')
-//   })
-// })
+import React from 'react'
+import {shallow} from 'enzyme'
+import {expect} from 'chai'
+import Footer from '../src/components/Footer'
+
+describe('<Footer />', () => {
+  const primProps = {
+    author: ''
+  }
+  it('render without crashing', () => {
+    const wrapper = shallow(<Footer {...primProps} />)
+    expect(wrapper.length).to.be.equal(1)
+  })
+  it('Did the component render the author prop', () => {
+    const wrapper = shallow(<Footer author='paulo' />)
+    expect(wrapper.contains(<p>Feito por: paulo</p>)).to.be.equal(true)
+  })
+  it('Did the component have a author property', () => {
+    expect(Footer.propTypes).to.have.property('author')
+  })
+  // it('Did the component return an error if author prop is not a string', () => {
+  //   const wrapper = shallow(<Footer author={2} />)
+  //   const before = wrapper.unrendered.props.author
+  //   expect(wrapper).to.throw()
+  // })
+})
