476aae9195a75eecd0e43ada35aa027a6d9afbe0
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2017-06-05 18:14:31 -0300

    testing and improving the card-footer component

--- src/components/Card/card-footer.js
+++ src/components/Card/card-footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const CardFooter = ({total_value, website}) => (
   <div className='cards__card--footer'>
@@ -10,5 +11,10 @@ const CardFooter = ({total_value, website}) => (
     </div>
   </div>
 )
+
+CardFooter.propTypes = {
+  total_value: PropTypes.number.isRequired,
+  website: PropTypes.string.isRequired
+}
 
 export default CardFooter
--- tests/card-footer.test.js
+++ tests/card-footer.test.js
@@ -1,12 +1,24 @@
-// import React from 'react'
-// import {render} from 'enzyme'
-// import {expect} from 'chai'
-//
-// import CardFooter from '../src/components/Card/card-footer'
-//
-// describe('<CardFooter />', () => {
-//   const wrapper = render(<CardFooter total_value={234.45} website='#' />)
-//   it('should component exist', () => {
-//     expect(wrapper).to.exist
-//   })
-// })
+import React from 'react'
+import {render} from 'enzyme'
+import {expect} from 'chai'
+import CardFooter from '../src/components/Card/card-footer'
+
+describe('<CardFooter />', () => {
+  const primProps = {
+    total_value: 234.45,
+    website: '#'
+  }
+  const wrapper = render(<CardFooter {...primProps} />)
+  it('should component exist', () => {
+    expect(wrapper).to.exist
+  })
+  it('render without crashing', () => {
+    expect(wrapper.length).to.be.equal(1)
+  })
+  it('did the total_value property exists on the proptype of the component?', () => {
+    expect(CardFooter.propTypes).have.a.property('total_value')
+  })
+  it('did the website property exists on the proptype of the component?', () => {
+    expect(CardFooter.propTypes).have.a.property('website')
+  })
+})
