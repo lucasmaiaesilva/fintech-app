b3ec0fa0b55ba8eb182d39a9ac96c499b6472105
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2017-06-05 18:04:48 -0300

    improving the tests on card-header

--- tests/Card.test.js
+++ tests/Card.test.js
@@ -1,12 +1,16 @@
-// import React from 'react'
-// import {shallow} from 'enzyme'
-// import {expect} from 'chai'
-// import Card from '../src/components/Card'
-// import CardHeader from '../src/components/Card/card-header'
-// import CardBody from '../src/components/Card/card-body'
-// import CardFooter from '../src/components/Card/card-footer'
-//
-// describe('<Card />', () => {
+import React from 'react'
+import {shallow} from 'enzyme'
+import {expect} from 'chai'
+import Card from '../src/components/Card'
+import CardHeader from '../src/components/Card/card-header'
+import CardBody from '../src/components/Card/card-body'
+import CardFooter from '../src/components/Card/card-footer'
+
+describe('<Card />', () => {
+  it('component render without crash', () => {
+    const wrapper = shallow(<Card />)
+    expect(wrapper.length).to.be.equal(1)
+  })
 //   it('PropTypes of object props exist', () => {
 //     expect(Card.propTypes).to.exist
 //   })
@@ -19,4 +23,4 @@
 //     ])
 //     expect(before).to.be.equal(true)
 //   })
-// })
+})
--- tests/card-header.test.js
+++ tests/card-header.test.js
@@ -1,11 +1,24 @@
-// import React from 'react'
-// import {render} from 'enzyme'
-// import {expect} from 'chai'
-// import CardHeader from '../src/components/Card/card-header'
-//
-// describe('<CardHeader />', () => {
-//   const wrapper = render(<CardHeader name='peso' flag='argentina-flag' />)
-//   it('should component exist', () => {
-//     expect(wrapper).to.exist
-//   })
-// })
+import React from 'react'
+import {render} from 'enzyme'
+import {expect} from 'chai'
+import CardHeader from '../src/components/Card/card-header'
+
+describe('<CardHeader />', () => {
+  const primProps = {
+    name: 'peso',
+    flag: 'argentina-flag'
+  }
+  const wrapper = render(<CardHeader {...primProps} />)
+  it('should component exist', () => {
+    expect(wrapper).to.exist
+  })
+  it('render without crashing', () => {
+    expect(wrapper.length).to.be.equal(1)
+  })
+  it('did the name property exists on the proptype of the component?', () => {
+    expect(CardHeader.propTypes).have.a.property('name')
+  })
+  it('did the flag property exists on the proptype of the component?', () => {
+    expect(CardHeader.propTypes).have.a.property('flag')
+  })
+})
